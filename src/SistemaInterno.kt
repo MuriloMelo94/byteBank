7ebacca94149f36fd37f88170f025fff4ccaf1a2
Murilo Melo <murilomelo94@outlook.com>
2023-05-03 15:56:14 -0300

    test :poop: testando e aprendendo mais situações problemáticas ao se utilizar herança sem os devidos cuidados.

--- src/SistemaInterno.kt
+++ src/SistemaInterno.kt
@@ -3,7 +3,8 @@ class SistemaInterno {
     fun entra(gerente: FuncionarioAdmin, senha: Int){
         if (gerente.autentica(senha)){
             println("Bem-Vindo ao ByteBank")
+        } else {
+            println("Falha na autenticação")
         }
-        println("Falha na autenticação")
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,9 @@
 fun main() {
 
+    testaFuncionarioAdmin()
+}
+
+private fun testaFuncionarioAdmin() {
     val gerente = Gerente(
         nome = "Murilo",
         cpf = "111.111.111-11",
@@ -14,4 +18,15 @@ fun main() {
         senha = 2000,
         plr = 200.0
     )
+
+    val cliente = Cliente(
+        nome = "Gui",
+        cpf = "333.333.333-33",
+        senha = 1234
+    )
+
+    val sistema = SistemaInterno()
+    sistema.entra(gerente, 1000)
+    sistema.entra(diretor, 2000)
+//    sistema.entra(cliente, 1234)
 }
