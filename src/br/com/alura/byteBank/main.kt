d8e5bdabb311320c2bef159992a30a71ae5de847
Murilo Melo <murilomelo94@outlook.com>
2023-05-12 10:27:42 -0300

    feat :alembic: experimentando object expression no código

--- src/br/com/alura/byteBank/main.kt
+++ src/br/com/alura/byteBank/main.kt
@@ -1,12 +1,25 @@
 package br.com.alura.byteBank
 
-import br.com.alura.byteBank.Modelo.Cliente
-import br.com.alura.byteBank.Modelo.ContaCorrente
-import br.com.alura.byteBank.Modelo.totalContas
+import br.com.alura.byteBank.Modelo.*
 import br.com.alura.byteBank.Teste.testaContasDiferentes
 import br.com.alura.byteBank.Teste.testaFuncionarioAdmin
 
 fun main() {
+
+    val fran = object : Autenticavel {
+        val nome = "Fran"
+        val cpf = "444.333.444-33"
+        val senha = 1000
+
+        override fun autentica (senha : Int) = this.senha == senha
+
+    }
+
+    val sistemaInterno = SistemaInterno()
+    sistemaInterno.entra(fran, 1000)
+
+    println("Nome do novo usuário ${fran.nome}")
+    println("CPF do novo usuário ${fran.cpf}")
 
     val fernanda = Cliente(nome = "Fernanda", cpf = "654.654.654-65", senha = 1234)
     val contaCorrente = ContaCorrente(titular = fernanda, numero = 3003)
