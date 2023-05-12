9984e7ceb067545885ddb51b89d26e977f648827
Murilo Melo <murilomelo94@outlook.com>
2023-05-12 10:14:00 -0300

    feat :sparkles: adiciona variável global para contagem de contas totais criadas no programa.

--- src/br/com/alura/byteBank/Modelo/Conta.kt
+++ src/br/com/alura/byteBank/Modelo/Conta.kt
@@ -1,11 +1,19 @@
 package br.com.alura.byteBank.Modelo
 
+var totalContas: Int = 0
+    private set
+
 abstract class Conta(
     val titular: Cliente,
     val numero: Int
 ) {
     var saldo = 0.0
         protected set
+
+    init {
+        println("*** Criando Nova Conta ***")
+        totalContas++
+    }
 
     fun deposita(valor: Double) {
         if (valor > 0.0) {
--- src/br/com/alura/byteBank/main.kt
+++ src/br/com/alura/byteBank/main.kt
@@ -1,12 +1,19 @@
 package br.com.alura.byteBank
 
+import br.com.alura.byteBank.Modelo.Cliente
+import br.com.alura.byteBank.Modelo.ContaCorrente
+import br.com.alura.byteBank.Modelo.totalContas
 import br.com.alura.byteBank.Teste.testaContasDiferentes
 import br.com.alura.byteBank.Teste.testaFuncionarioAdmin
 
 fun main() {
 
+    val fernanda = Cliente(nome = "Fernanda", cpf = "654.654.654-65", senha = 1234)
+    val contaCorrente = ContaCorrente(titular = fernanda, numero = 3003)
     testaFuncionarioAdmin()
     testaContasDiferentes()
+
+    println("Total de contas criadas $totalContas")
 
 }
 
